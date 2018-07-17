cd80f457028d25fba45a7beeb59eb800458144c3
Felipe Stein <projectstein@gmail.com>
2018-07-17 15:36:13 -0300

    Projeto Backend_SASC criado

---
+++ src/main/java/com/projectstein/backend_sasc/BackendSascApplication.java
@@ -0,0 +1,12 @@
+package com.projectstein.backend_sasc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSascApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSascApplication.class, args);
+	}
+}

8b148a66ebabf867a9a09049dd783e921eb873b9
projectstein <projectstein@gmail.com>
2018-07-17 21:14:35 -0300

    Banco de teste configurado

--- src/main/java/com/projectstein/backend_sasc/domain/CriticaSugestaoElogio.java
+++ src/main/java/com/projectstein/backend_sasc/domain/CriticaSugestaoElogio.java
@@ -3,9 +3,18 @@ package com.projectstein.backend_sasc.domain;
 import java.io.Serializable;
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
 public class CriticaSugestaoElogio implements Serializable {
 		private static final long serialVersionUID = 1L;
 		
+		@Id
+		@GeneratedValue(strategy=GenerationType.IDENTITY)
 		private Integer id;
 		private String comentario;
 		private Date dataComentario;
@@ -17,7 +26,6 @@ public class CriticaSugestaoElogio implements Serializable {
 
 
 		public CriticaSugestaoElogio(Integer id, String comentario, Date dataComentario) {
-			super();
 			this.id = id;
 			this.comentario = comentario;
 			this.dataComentario = dataComentario;

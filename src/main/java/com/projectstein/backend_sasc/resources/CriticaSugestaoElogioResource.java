c760495a0f85edb41c5351ee473a6684d0ad0b37
projectstein <projectstein@gmail.com>
2018-08-27 10:08:34 -0300

    Classes de Tratamento de exceções testadas e preparação para o metodo post efetuada

--- src/main/java/com/projectstein/backend_sasc/resources/CriticaSugestaoElogioResource.java
+++ src/main/java/com/projectstein/backend_sasc/resources/CriticaSugestaoElogioResource.java
@@ -1,11 +1,15 @@
 package com.projectstein.backend_sasc.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.projectstein.backend_sasc.domain.CriticaSugestaoElogio;
 import com.projectstein.backend_sasc.services.CriticaSugestaoElogioService;
@@ -22,6 +26,15 @@ public class CriticaSugestaoElogioResource {
 		CriticaSugestaoElogio obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 
+	}
+	
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody CriticaSugestaoElogio obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 
 }
--- src/main/java/com/projectstein/backend_sasc/services/CriticaSugestaoElogioService.java
+++ src/main/java/com/projectstein/backend_sasc/services/CriticaSugestaoElogioService.java
@@ -5,16 +5,26 @@ import org.springframework.stereotype.Service;
 
 import com.projectstein.backend_sasc.domain.CriticaSugestaoElogio;
 import com.projectstein.backend_sasc.repository.CriticaSugestaoElogioRepository;
+import com.projectstein.backend_sasc.services.exception.ObjectNotFoundException;
 
 @Service
-	public class CriticaSugestaoElogioService {
-		
-		@Autowired
-		private CriticaSugestaoElogioRepository repo;
-		
-		public CriticaSugestaoElogio find(Integer id) {
-			CriticaSugestaoElogio obj = repo.findOne(id);
-			return obj;
+public class CriticaSugestaoElogioService {
+
+	@Autowired
+	private CriticaSugestaoElogioRepository repo;
+
+	public CriticaSugestaoElogio find(Integer id) {
+		CriticaSugestaoElogio obj = repo.findOne(id);
+		if (obj == null) {
+			throw new ObjectNotFoundException(
+					"Objeto não encontrado! Id: " + id + ", Tipo: " + CriticaSugestaoElogio.class.getName());
 		}
+		return obj;
+	}
+
+	public CriticaSugestaoElogio insert(CriticaSugestaoElogio obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
 
 }
